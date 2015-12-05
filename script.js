f2f2aa143a30b7636ed1d8b4c2013464215a7627
pedrada <antonio.vianna.pd@gmail.com>
2015-12-05 17:20:00 -0200

    Width correction 3

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(function(){
 	  url: ("https://api.twitter.com/1/statuses/oembed.json?url="+url),
 	  success: function( data ) {
 	    $('.tuite').html(data.html);
-		
+		$('iframe')[0].style = ('position: static; visibility: visible; display: block; width: 100%; height: 159.766px; padding: 0px; border: none; max-width: 170px; min-width: 160px; margin-top: 10px; margin-bottom: 10px;');
 	  }
 	});
 
